3498ec786c39804a5dc591cbe2d4b17cc0b3447d
Richard Schilling <richard@aesirmachina.com>
2023-07-14 13:15:51 -0700

    edit code for compilation.

--- mobile/android/irc-core/src/main/java/gov/nasa/gsfc/irc/devices/ports/adapters/AbstractMessageInputAdapter.java
+++ mobile/android/irc-core/src/main/java/gov/nasa/gsfc/irc/devices/ports/adapters/AbstractMessageInputAdapter.java
@@ -26,6 +26,7 @@ package gov.nasa.gsfc.irc.devices.ports.adapters;
 
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
