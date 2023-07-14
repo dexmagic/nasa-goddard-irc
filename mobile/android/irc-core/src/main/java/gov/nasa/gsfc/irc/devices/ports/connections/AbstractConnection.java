c088145ab64429fca54e8835118e3deb258fb5ef
Richard Schilling <richard@aesirmachina.com>
2023-07-14 13:15:51 -0700

    edit code for compilation.

--- mobile/android/irc-core/src/main/java/gov/nasa/gsfc/irc/devices/ports/connections/AbstractConnection.java
+++ mobile/android/irc-core/src/main/java/gov/nasa/gsfc/irc/devices/ports/connections/AbstractConnection.java
@@ -33,6 +33,7 @@ import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
 import java.util.Iterator;
 import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
