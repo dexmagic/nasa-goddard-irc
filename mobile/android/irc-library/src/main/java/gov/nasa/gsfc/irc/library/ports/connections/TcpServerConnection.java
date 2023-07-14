f413d74107358931d52774e90ef97c7d513c3084
Richard Schilling <richard@aesirmachina.com>
2023-07-14 13:34:12 -0700

    edit code for compilation.

--- mobile/android/irc-library/src/main/java/gov/nasa/gsfc/irc/library/ports/connections/TcpServerConnection.java
+++ mobile/android/irc-library/src/main/java/gov/nasa/gsfc/irc/library/ports/connections/TcpServerConnection.java
@@ -35,6 +35,7 @@ import java.nio.channels.SocketChannel;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
