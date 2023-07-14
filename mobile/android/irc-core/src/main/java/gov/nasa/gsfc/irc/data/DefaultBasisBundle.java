0ae0f2084f723346948cecc9a882336632a68222
Richard Schilling <richard@aesirmachina.com>
2023-07-14 13:15:50 -0700

    edit code for compilation.

--- mobile/android/irc-core/src/main/java/gov/nasa/gsfc/irc/data/DefaultBasisBundle.java
+++ mobile/android/irc-core/src/main/java/gov/nasa/gsfc/irc/data/DefaultBasisBundle.java
@@ -30,6 +30,7 @@ import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
